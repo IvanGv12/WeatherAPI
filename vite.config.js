ae7af07742866c9e3e0691a2e243f5a6f517aeed
IvanGv12 <ivangomva13@gmail.com>
2025-12-08 15:55:06 -0600

    Configure Vite with @vitejs/plugin-vue to process .vue files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'; // <-- Importación necesaria
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+        }),
+        vue({ // <-- ¡Activación del plugin!
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
