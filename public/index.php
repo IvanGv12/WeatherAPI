44842b2b24aa1d0c90b2486bd9feebce48596558
IvanGv12 <ivangomva13@gmail.com>
2025-12-11 03:56:25 -0600

    force deploy

--- public/index.php
+++ public/index.php
@@ -1,22 +1,39 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Maintenance mode
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+/*
+|--------------------------------------------------------------------------
+| Check If Application Is Under Maintenance
+|--------------------------------------------------------------------------
+*/
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Composer autoload
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+*/
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+*/
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Handle request
-$app->handleRequest(
-    Request::capture()
-)->send();
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
